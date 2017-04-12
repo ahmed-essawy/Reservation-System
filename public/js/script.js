4d4c817f29af7f84f11cf851ae2e7a49a92afa6e
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-04-12 16:16:54 +0200

    Add css & JS files

--- public/js/script.js
+++ public/js/script.js
@@ -2,33 +2,13 @@
     if (localStorage.getItem("User")) {
         $("#loginbtn > li").toggle();
         $("#loginbtn > li #username").text(JSON.parse(localStorage.getItem("User")).name);
-        if (window.location.pathname == '/login' || window.location.pathname == '/register')
+        if (window.location.hash == '#/login' || window.location.hash == '#/register')
             window.location = "/admincp";
     }
 })
-function login() {
-    var data = $("#loginform").serializeArray();
-    $.post('loginuser', { username: data[0]['value'], password: data[1]['value'] }, data => {
-        if (data.correct) {
-            localStorage.setItem("User", JSON.stringify(data.user));
-            window.location = "/admincp";
-        } else {
-            $("#loginform > div").html("<h5 class=\"alert alert-danger\">Username or password is wrong</h5>");
-        }
-    });
-}
 function logout() {
     localStorage.removeItem("User");
     window.location = "/";
-}
-function register() {
-    var data = $("#loginform").serializeArray();
-    $.post('registeruser', { name: data[0]['value'], username: data[1]['value'], password: data[2]['value'] }, data => {
-        if (data.correct) {
-            localStorage.setItem("User", JSON.stringify(data.user));
-            window.location = "/admincp";
-        }
-    });
 }
 function html_resp(msg, cssClass) {
     $("#response").text(msg).addClass(cssClass).fadeOut(4000, () => {
