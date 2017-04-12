87f7d5df2b3b93da6db2f229c0948c19c469bcfe
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-04-12 16:16:39 +0200

    Build Services

---
+++ public/js/Services/LocalStorage.js
@@ -0,0 +1,9 @@
+﻿App.factory('LocalStorage', ['$rootScope', function ($rootScope) {
+    return {
+        set: (key, value) => {
+            delete value.password; delete value.pass2;
+            localStorage.setItem(key, JSON.stringify(value));
+        },
+        get: key => JSON.parse(localStorage.getItem(key))
+    }
+}]);
--- public/js/Services/Services.js
+++ public/js/Services/Services.js
@@ -1,4 +1,4 @@
-﻿App.factory('API', ["$http", function ($http) {
+App.factory('API', ["$http", function ($http) {
     return {
         Employees: $http.get('http://localhost:3685/API/Employees'),
         AddEmp: object => $http({ method: 'POST', url: 'http://localhost:3685/API/Employee', data: object, headers: { "Content-Type": "application/json;charset=utf-8" } }),
@@ -8,5 +8,14 @@
         AddUser: object => $http({ method: 'POST', url: 'http://localhost:3685/API/User', data: object, headers: { "Content-Type": "application/json;charset=utf-8" } }),
         EditUser: object => $http({ method: 'PUT', url: 'http://localhost:3685/API/User', data: object, headers: { "Content-Type": "application/json;charset=utf-8" } }),
         SendMsg: object => $http({ method: 'POST', url: 'http://localhost:3685/API/Message', data: object, headers: { "Content-Type": "application/json;charset=utf-8" } })
+    }
+}]);
+App.factory('LocalStorage', ['$rootScope', function ($rootScope) {
+    return {
+        set: (key, value) => {
+            delete value.password; delete value.pass2;
+            localStorage.setItem(key, JSON.stringify(value));
+        },
+        get: key => JSON.parse(localStorage.getItem(key))
     }
 }]);
